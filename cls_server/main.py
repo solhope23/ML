23c3b33fd57b4786f27130bafeef7acb1380ec40
solhope23 <S526413103@gmail.com>
2025-07-28 17:47:22 +0300

    Update main.py cls

--- cls_server/main.py
+++ cls_server/main.py
@@ -6,18 +6,38 @@ from core.cls import CLS
 from fastapi import Request
 import requests
 import os
+import time
 
+
+def wait_for_model(timeout=30):
+    url = f"http://{host}:{port}/health"
+
+    for i in range(timeout):
+        try:
+            r = requests.get(url, timeout=1)
+            if r.status_code == 200:
+                print("model_server is ready.")
+                return
+        except Exception as e:
+            print(f"model_server not ready yet: {e}")
+        print(f"Waiting for model_server... ({i + 1}/{timeout})")
+        time.sleep(1)
+
+    raise RuntimeError("model_server not available after timeout.")
 
 app = FastAPI()
 
 model: Optional[Model] = None
 
+host = os.getenv("MODEL_SERVER_HOST", "localhost")
+port = os.getenv("MODEL_SERVER_PORT", "8000")
+
 @app.on_event("startup")
 def fetch_model():
     global model
     try:
-        host = os.getenv("MODEL_SERVER_HOST", "localhost")
-        response = requests.get(f"http://{host}:8000/get-model")
+        wait_for_model()
+        response = requests.get(f"http://{host}:{port}/get-model")
         response.raise_for_status()
         model = Model(**response.json())
         print("Model fetched from model_server")
@@ -30,10 +50,17 @@ def fetch_model():
 @app.get("/form", response_class=HTMLResponse)
 def form():
 
-    html = "<h2>Classify Instance</h2><form method='post'>"
+    target_col = model.target_col
+    target_values = model.model_schema.get(target_col, [])
+
+    html = (
+        "<h2>Classify Instance</h2>"
+        f"<h4>Target column: <u>{target_col}</u> &rarr; {target_values}</h4>"
+        "<form method='post'>"
+    )
 
     for feature, values in model.model_schema.items():
-        if feature != model.target_col:
+        if feature != target_col:
             html += f"<label>{feature}</label><br>"
             html += f"<select name='{feature}'>"
             for val in values:
